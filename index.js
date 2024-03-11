146d575e88caf4175b9ed46a237b1cb50aff33f1
ManishaG2023 <139097301+ManishaG2023@users.noreply.github.com>
2024-03-11 21:57:29 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
-let map;
+/*!
+ * toidentifier
+ * Copyright(c) 2016 Douglas Christopher Wilson
+ * MIT Licensed
+ */
 
-function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 40.712, lng: -74.00 },
-    zoom: 12,
-  });
- // var marker = new google.maps.Marker({ position: {lat: 40.712, lng: -74.00 },
- // map: map, label: {text: "Mario's Pizza", fontSize: '20px'}})
+'use strict'
+
+/**
+ * Module exports.
+ * @public
+ */
+
+module.exports = toIdentifier
+
+/**
+ * Trasform the given string into a JavaScript identifier
+ *
+ * @param {string} str
+ * @returns {string}
+ * @public
+ */
+
+function toIdentifier (str) {
+  return str
+    .split(' ')
+    .map(function (token) {
+      return token.slice(0, 1).toUpperCase() + token.slice(1)
+    })
+    .join('')
+    .replace(/[^ _0-9a-z]/gi, '')
 }
-
-window.initMap = initMap;
